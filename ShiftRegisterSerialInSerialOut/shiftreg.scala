cc13b98815a31475451dc3ea4ea7e73cb8a541ad
Aya Elnaggar <AyaElnaggar@aucegypt.edu>
2018-12-04 01:27:59 +0200

    shiftreg.scala compiled

--- ShiftRegisterSerialInSerialOut/shiftreg.scala
+++ ShiftRegisterSerialInSerialOut/shiftreg.scala
@@ -1,3 +1,6 @@
+//LINK USED: https://github.com/ucb-bar/chisel-tutorial/blob/release/src/main/scala/solutions/VecShiftRegisterParam.scala
+package solutions
+
 import chisel3._
 
 class shiftreg extends Module{
@@ -7,8 +10,14 @@ class shiftreg extends Module{
     val Clock = Input(UInt(1.W))
   })
 
-  // ?? do i need initializaton
-  val Q = RegNext(Cat(Q(2,0), io.SI))
+  val initValues = Seq.fill(4) { 0.U(1.W) }
+  val delays = RegInit(VecInit(initValues))
 
-  io.SO := Q(3)
+  for (i <- 3 to 1 by -1) {
+    delays(i) := delays(i - 1)
+  }
+
+  delays(0) := io.SI
+  io.SO := delays(3)
+
 }
